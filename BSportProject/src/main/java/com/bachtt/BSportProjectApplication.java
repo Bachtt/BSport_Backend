2ef5efc3e79aec864d79e02fca9dbf9ab8dc105d
Bachtt <Bachtt107@gmail.com>
2022-04-10 09:52:30 +0700

    Create BSport Project

---
+++ BSportProject/src/main/java/com/bachtt/BSportProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bachtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BSportProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BSportProjectApplication.class, args);
+	}
+
+}
---
+++ BSportProject/src/test/java/com/bachtt/BSportProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bachtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BSportProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
